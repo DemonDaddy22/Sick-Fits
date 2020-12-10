509cd95c2049f5a24d37148c1f8e3628e652300f
Rohan Gupta <guptarohan369@gmail.com>
2020-12-10 23:36:53 +0530

    add start server config

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,16 @@
 require('dotenv').config({ path: 'variables.env' });
+
+import createServer from './createServer';
+import db from './db';
+
+const server = new createServer();
+
+// TODO - use Express middleware to handle cookies (JWT)
+// TODO - use Express middleware to populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    },
+}, deets => console.log(`Server is now running on http://localhost:${deets.port}`));
