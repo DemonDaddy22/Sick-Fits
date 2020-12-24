650b98815f9dbd28b441e4071b17f8d8a2dd855c
Rohan Gupta <guptarohan369@gmail.com>
2020-12-24 16:49:50 +0530

    add skip, first and orderBy to all items query for pagination to work

--- sick-fits/frontend/pages/shop.js
+++ sick-fits/frontend/pages/shop.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from '../components/Item';
 import Pagination from '../components/Pagination';
+import { perPage } from '../config';
 
 export const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+        items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
             id
             title
             price
@@ -34,7 +35,7 @@ export default class Shop extends React.Component {
 
     render = () => <TextCenter>
         <Pagination page={parseFloat(this.props.query.page) || 1} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query query={ALL_ITEMS_QUERY} variables={{ skip: this.props.query.page * perPage - perPage }}>
             {({ data, error, loading }) => {
                 if (loading) return <p>Loading...</p>;
                 if (error) return <p>Error: {error.message}</p>;
